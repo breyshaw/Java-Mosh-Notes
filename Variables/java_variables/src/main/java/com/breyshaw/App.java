b14b1e7a1c5f155b3f469089add6a06af335609f
breyshaw <dawsonbrey@gmail.com>
2022-02-08 13:42:06 -0500

    initial commit, variable notes

---
+++ Variables/java_variables/src/main/java/com/breyshaw/App.java
@@ -0,0 +1,20 @@
+package com.breyshaw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //Variables temporarily store data
+        //Initializing the variables. Variables are written in camelCase
+        int myAge = 27;
+        //After initialization you can change it again in the code
+        // int myAge = 28
+        //Declare one variable per line (it's cleaner)
+        int herAge = 29;
+        System.out.println( herAge );
+    }
+}
