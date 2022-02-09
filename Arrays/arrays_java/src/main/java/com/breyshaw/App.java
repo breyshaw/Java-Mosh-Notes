ae4ce72d2b445d533b9e377462e3986faa237637
breyshaw <dawsonbrey@gmail.com>
2022-02-09 12:15:57 -0500

    add multi dimensional array notes

--- Arrays/arrays_java/src/main/java/com/breyshaw/App.java
+++ Arrays/arrays_java/src/main/java/com/breyshaw/App.java
@@ -19,10 +19,24 @@ public class App
     // }
 
     //Newer syntax to initialize an array if you know what is already in it
-        public static void main( String[] args )
+    //     public static void main( String[] args )
+    // {
+    //     int[] numbers = {2,3,5,1,4};
+    //     Arrays.sort(numbers);
+    //     System.out.println(Arrays.toString(numbers));
+    // }
+
+    //MULTI DIMENSIONAL ARRAYS-------------------------------------
+    //2d arrays are used to store a matrix, 3d are used to store data of a cube
+            public static void main( String[] args )
     {
-        int[] numbers = {2,3,5,1,4};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
+        //Two rows, three columns
+        //2d
+        // int[][] numbers = new int[2][3];
+        // numbers[0][0] = 1;
+        //3D
+        int [][] numbers = {{1,2,3},{4,5,6} };
+        //use deepToString to print multi dimensional arrays
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
