bc94efa47c9549147eafc45965174d934c82a4b6
breyshaw <dawsonbrey@gmail.com>
2022-02-14 11:39:30 -0500

    add constants notes

---
+++ Constants/java_constants/src/main/java/com/breyshaw/App.java
@@ -0,0 +1,14 @@
+package com.breyshaw;
+
+public class App 
+{
+    //Constant Variable values cannot be changed
+    //The final command sets the variable as a constant
+    public static void main( String[] args )
+    {
+        final float pi = 3.14F;
+        //Cannot change this value or an error will occur
+        // pi = 1;
+        System.out.println( pi );
+    }
+}
