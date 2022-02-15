7a2ed2f3e581811abb88c75c96ccd6958657342e
breyshaw <dawsonbrey@gmail.com>
2022-02-15 13:00:22 -0500

    add input code and notes

---
+++ Reading_Input/java_reading_input/src/main/java/com/breyshaw/App.java
@@ -0,0 +1,24 @@
+package com.breyshaw;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //To work in the terminal window we use System.in
+        Scanner scanner = new Scanner(System.in);
+
+        //BYTES
+        // System.out.print("Age:");
+        //There are a lot of methods for reading data using the scanner object, next lets you choose the next thing to read, boolean, byte, etc
+        // byte age = scanner.nextByte(); //nextByte returns a byte value so we can store it in a byte variable
+
+        //STRINGS
+        //to read a string just use scanner.next()
+        System.out.print("Name:");
+        // String name = scanner.next(); //next method only reads one token. if using my first and last name it will only come back brey.
+        String name = scanner.nextLine(); //nextLine method reads the entire line. Can chain the trim method here if I want to get rid of whitespace.
+        System.out.println("You are " + name); //concatinating a string with a byte, Java automatically converts the byte to a string. Implicit conversion.
+    }
+}
