b1c4f8b11de47834682c7ce941c6ff935e4d3b5a
breyshaw <dawsonbrey@gmail.com>
2022-02-21 13:42:44 -0500

    add operator code and notes

--- Comparision_Operators/java_comparison_operators/src/main/java/com/breyshaw/App.java
+++ Comparision_Operators/java_comparison_operators/src/main/java/com/breyshaw/App.java
@@ -4,6 +4,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //COMPARISION OPERATORS
         //We use these operators to compare two primitive values
         int x = 1;
         int y = 1;
@@ -14,5 +15,26 @@ public class App
         System.out.println(x >= y);
         System.out.println(x < y);
         System.out.println(x <= y);
+
+        //LOGICAL OPERATORS
+
+        //AND Operator
+        // int temperature = 22;
+        // boolean isWarm = temperature > 20 && temperature < 30; //if both of these conditions are true, the result will be true, if either is false, so will the result
+        // System.out.println(isWarm);
+
+        //OR Operator
+        // boolean hasHighIncome = true;
+        // boolean hasGoodCredit = true;
+        // boolean isEligible = hasHighIncome || hasGoodCredit;
+        // System.out.println(isEligible);
+
+        //NOT operator
+        boolean hasHighIncome = true;
+        boolean hasGoodCredit = true;
+        boolean hasCriminalRecord = false;
+        boolean isEligible = (hasHighIncome || hasGoodCredit) && !hasCriminalRecord;
+        System.out.println(isEligible);
+
     }
 }
