2e39784e436d2124c47ecad63b612999c4a870e1
breyshaw <dawsonbrey@gmail.com>
2022-02-23 14:58:47 -0500

    add if statement code

--- if_statements/java_if_statements/src/main/java/com/breyshaw/App.java
+++ if_statements/java_if_statements/src/main/java/com/breyshaw/App.java
@@ -3,7 +3,7 @@ package com.breyshaw;
 public class App {
     public static void main(String[] args) {
 
-        int temp = 32; // declaring the variable
+        int temp = 35; // declaring the variable
 
         if (temp > 30) { // if this if statement condition is true, the following code block will
                          // execute. This if statement has 3 clauses. Parent followed by two children.
@@ -14,6 +14,16 @@ public class App {
             System.out.println("Beautiful day");
         } else {
             System.out.println("Cold day!");
+        
+        //Simplifying if statements -------------------------------------
+
+        int income = 50_000; //the underscore just makes the code more readable
+        // boolean hasHighIncome = false;//giving the variable an initial value. Makes it to where we don't need the else clause
+        // if (income > 100_000) { //variables can only be defined within a code block. Brackets required here if it is one statement.
+        //     hasHighIncome = true; //this is only available within this code block. So we declare the hasHighIncome variable before the if statement like on line 21
+        // }
+        boolean hasHighIncome = (income > 100_000); //all the code above can be reduced to this one line
+        System.out.println(hasHighIncome);
         }
-    }
-}
+    }}
+
