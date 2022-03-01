545d4046339bff3a8e4fb4dc2e61174f7dae5238
breyshaw <dawsonbrey@gmail.com>
2022-03-01 14:39:47 -0500

    add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Java Notes
+This is my personal code and notes taken while following along with Mosh's Java Series.
+Source Video [here!](https://www.youtube.com/watch?v=eIrMbAQSU34)
--- Reading_Input/java_reading_input/src/main/java/com/breyshaw/App.java
+++ Reading_Input/java_reading_input/src/main/java/com/breyshaw/App.java
@@ -10,15 +10,16 @@ public class App
         Scanner scanner = new Scanner(System.in);
 
         //BYTES
-        // System.out.print("Age:");
+        System.out.print("Age:");
         //There are a lot of methods for reading data using the scanner object, next lets you choose the next thing to read, boolean, byte, etc
-        // byte age = scanner.nextByte(); //nextByte returns a byte value so we can store it in a byte variable
+        byte age = scanner.nextByte(); //nextByte returns a byte value so we can store it in a byte variable
+        System.out.println("You are " + age + "years old.");
 
         //STRINGS
         //to read a string just use scanner.next()
-        System.out.print("Name:");
+        // System.out.print("Name:");
         // String name = scanner.next(); //next method only reads one token. if using my first and last name it will only come back brey.
-        String name = scanner.nextLine(); //nextLine method reads the entire line. Can chain the trim method here if I want to get rid of whitespace.
-        System.out.println("You are " + name); //concatinating a string with a byte, Java automatically converts the byte to a string. Implicit conversion.
+        // String name = scanner.nextLine(); //nextLine method reads the entire line. Can chain the trim method here if I want to get rid of whitespace.
+        // System.out.println("You are " + name); //concatinating a string with a byte, Java automatically converts the byte to a string. Implicit conversion.
     }
 }
